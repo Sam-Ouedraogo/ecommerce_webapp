e97b6422c48640d10fe35fce698465d7e562d32b
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2025-03-10 04:26:07 -0400

    Initial commit

---
+++ marketplace/core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from item.models import Category, Item
+
+# Create your views here.
+
+# show the newest 6 items
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    category = Category.objects.all() # get all the categories
+    
+    
+    return render(request, 'core/index.html', {
+        'category': category, # These last 2 lines allow using the variable in the template. They point back to the predefined variables
+        'items': items
+    })
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def contact (request):
+    return render(request, 'core/contact.html')
---
+++ marketplace/item/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+# Order items by name and adds the str representation of each item and category    
+    class Meta:
+        ordering = ('name' ,)
+        verbose_name_plural = 'Categories'
+        
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Item(models.Model):
+    Category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE) # If user deletes category, all items in that category are being deleted
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True) #uploads and create item_mage location on the server if it doesn't exist.
+    is_sold = models.BooleanField(default=False)
+    create_by = models.ForeignKey(User, related_name='items', on_delete = models.CASCADE) #If user deletes all items, delte all items
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
