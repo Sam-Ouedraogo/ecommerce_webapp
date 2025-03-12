510770a97d64f3f69bcb96ae87be4e4424edcc87
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2025-03-12 02:54:29 -0400

    User sign up added

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
 
 # Create your views here.
 
@@ -8,7 +10,6 @@ from item.models import Category, Item
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     category = Category.objects.all() # get all the categories
-    
     
     return render(request, 'core/index.html', {
         'category': category, # These last 2 lines allow using the variable in the template. They point back to the predefined variables
@@ -18,3 +19,19 @@ def index(request):
 
 def contact (request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login')
+    else:
+        form = SignupForm()
+    
+    return render(request, 'core/signup.html',{
+        'form': form
+    })
--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -21,11 +21,9 @@ from core import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index, contact
 
 urlpatterns = [
+    path('', include('core.urls')),
     path("admin/", admin.site.urls),
     path('items/', include('item.urls')),
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name = 'contact' )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
